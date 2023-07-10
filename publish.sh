4528398a2e8b51837f6c9fee5cedf2d016f6fc1a
tarneo <tarneo@tarneo.fr>
2023-07-10 20:05:21 +0200

    fix(publish script): make publishing actually work
    Before, publishing didn't work because Make doesn't keep variables between
    commands. Switched to using a script, publish.sh.
    (+ version bump to fully test)

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+cargo update
+cargo publish
+version=$(grep version Cargo.toml | cut -d "\"" -f 2 | head -n 1)
+major=$(echo "${version}" | cut -d "." -f 1)
+minor=$(echo "${version}" | cut -d "." -f 2)
+patch=$(echo "${version}" | cut -d "." -f 3)
+docker build -t "ghcr.io/tarneaux/shlonk:latest" .
+docker tag "ghcr.io/tarneaux/shlonk:latest" "ghcr.io/tarneaux/shlonk:${major}"
+docker tag "ghcr.io/tarneaux/shlonk:latest" "ghcr.io/tarneaux/shlonk:${major}.${minor}"
+docker tag "ghcr.io/tarneaux/shlonk:latest" "ghcr.io/tarneaux/shlonk:${major}.${minor}.${patch}"
+docker push "ghcr.io/tarneaux/shlonk:latest"
+docker push "ghcr.io/tarneaux/shlonk:${major}"
+docker push "ghcr.io/tarneaux/shlonk:${major}.${minor}"
+docker push "ghcr.io/tarneaux/shlonk:${major}.${minor}.${patch}"
+
